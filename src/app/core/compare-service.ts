77725abbc4bf857c1f15d4686061f0af5f57e397
Kevin Smith <kevinmilly@gmail.com>
2025-10-25 23:30:41 +0000

    change configs for deploy

--- src/app/core/compare-service.ts
+++ src/app/core/compare-service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ModelResult } from '../models/results-models';
+import { environment } from '../../environment.prod';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class CompareService {
-  private apiUrl = 'https://hallowed-cobweb-wr9wxqrp6442gvv7-3000.app.github.dev/compare'; // adjust if deployed
+   private readonly apiUrl = environment.API_URL;
 
   constructor(private http: HttpClient) {}
 
