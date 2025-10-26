f874410b5c58c8bbf84d6af49df620e910a9a0bd
Kevin Smith <kevinmilly@gmail.com>
2025-10-26 14:58:22 +0000

    fixing styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { Compare } from './features/compare/compare';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Compare],
-  templateUrl: './compare.html',
-  styleUrls: ['./compare.css'],
+  imports: [],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css'],
 })
 export class App { }
